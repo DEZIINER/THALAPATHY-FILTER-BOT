3b707b0b40410ecad565e54e6f821c671dcc9c00
DEZIINER <127255670+DEZIINER@users.noreply.github.com>
2023-09-09 20:33:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,13 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "5634679884:AAH6KjE4JbS5rD3ljyZSJZJx6I-rbz5
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/7e56d907542396289fee4.jpg https://telegra.ph/file/9aa8dd372f4739fe02d85.jpg https://telegra.ph/file/adffc5ce502f5578e2806.jpg https://telegra.ph/file/6937b60bc2617597b92fd.jpg https://telegra.ph/file/09a7abaab340143f9c7e7.jpg https://telegra.ph/file/5a82c4a59bd04d415af1c.jpg https://telegra.ph/file/323986d3bd9c4c1b3cb26.jpg https://telegra.ph/file/b8a82dcb89fb296f92ca0.jpg https://telegra.ph/file/31adab039a85ed88e22b0.jpg https://telegra.ph/file/c0e0f4c3ed53ac8438f34.jpg https://telegra.ph/file/eede835fb3c37e07c9cee.jpg https://telegra.ph/file/e17d2d068f71a9867d554.jpg https://telegra.ph/file/8fb1ae7d995e8735a7c25.jpg https://telegra.ph/file/8fed19586b4aa019ec215.jpg https://telegra.ph/file/8e6c923abd6139083e1de.jpg https://telegra.ph/file/0049d801d29e83d68b001.jp')).split() #SAMPLE PIC
+PICS = (environ.get('PICS', 'https://graph.org/file/5d6c75894017df75ba88e.jpg
+https://graph.org/file/8547f1d57d19e432ca8c3.jpg
+https://graph.org/file/f635863f497ae6f8c2009.jpg
+https://graph.org/file/a46f8fc289c0c873b7bb5.jpg
+https://graph.org/file/8620bd36652f635a4b77a.jpg
+https://graph.org/file/60ccb3cf3ca2b367b8ee5.jpg
+https://graph.org/file/b6c794063b841825be637.jpg')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
 MELCOW_IMG = environ.get("MELCOW_VID", "https://graph.org/file/411ffa270f82bac099d2d.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
